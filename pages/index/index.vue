ce9f4fe5f0c2b9b331ef81db1803fcd2da5d61ae
Kingsdom005 <75762130+Kingsdom005@users.noreply.github.com>
2022-01-27 19:02:15 +0800

    Add files via upload

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,7 +1,7 @@
 <template>
 	<view class="content">
 		<u-button type="primary" @click="scanToSignIn">扫码签到</u-button>
-		<u-button type="primary">扫码开始服务</u-button>
+		<u-button type="primary" @click="scanToStartService">扫码开始服务</u-button>
 		<u-button type="primary">我的工单</u-button>
 	</view>
 </template>
@@ -17,11 +17,35 @@
 		},
 		methods: {
 			scanToSignIn(){
+				let that = this
 				uni.scanCode({
 					onlyFromCamera:false,
 					scanType:['qrCode'],
 					success(res) {
+						 console.log(res)
+						that.$u.api.scanCode(res.result).then(res => {
+							uni.showToast({
+								title:"签到成功",
+								type:'success',
+							})
+						})
+					}
+				})
+			},
+			scanToStartService(){
+				let that = this
+				uni.scanCode({	
+					onlyFromCamera:false,
+					scanType:['qrCode'],
+					success(res) {
 						console.log(res)
+						console.log(res['result'])
+						that.$u.api.startService(res.result).then(res=>{
+							uni.showToast({
+								title:"成功开始服务",
+								type:'success',
+							})
+						})
 					}
 				})
 			}
