ee285bb26d367e3d4fb36e9e0ca541a8ba599894
Kingsdom005 <75762130+Kingsdom005@users.noreply.github.com>
2022-01-27 19:03:16 +0800

    Add files via upload

--- common/api.js
+++ common/api.js
@@ -4,8 +4,16 @@ const install = (Vue,vm) =>{
 	let login = (username,password)=>{
 		return vm.$u.post("/login",{username:username,password:CryptoJS.SHA512(password).toString()})
 	}
+	let scanCode = (uuid) =>{
+		return vm.$u.put("/scanCode",{uuid})
+	}
+	let startService = (uuid)=>{
+		return vm.$u.put("/startService",{uuid})
+	}
 	vm.$u.api = {
-		login
+		login,
+		scanCode,
+		startService,
 	}
 }
 export default {install}
