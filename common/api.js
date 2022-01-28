3551cd74c224d11cd0e634e441b978c40d881504
Kingsdom005 <75762130+Kingsdom005@users.noreply.github.com>
2022-01-28 23:38:34 +0800

    Add files via upload

--- common/api.js
+++ common/api.js
@@ -1,6 +1,9 @@
 import CryptoJS  from "../utils/crypto-js.min.js"
 
 const install = (Vue,vm) =>{
+	let register = (username,password)=>{
+		return vm.$u.post("/register",{username:username,password:CryptoJS.SHA512(password).toString()})
+	}
 	let login = (username,password)=>{
 		return vm.$u.post("/login",{username:username,password:CryptoJS.SHA512(password).toString()})
 	}
@@ -10,10 +13,22 @@ const install = (Vue,vm) =>{
 	let startService = (uuid)=>{
 		return vm.$u.put("/startService",{uuid})
 	}
+	let buildForm = (uuid,username)=>{
+		return vm.$u.post("/startService",{uuid:uuid,username:username})
+	}
+	// let buildForm = (uuid,username)=>{
+	// 	return vm.$u.post(`/startService?uuid=${uuid}&username=${username}`)
+	// }
+	let getMyAppointment = (username)=>{
+		return vm.$u.post("/getAppointment",{username})
+	}
 	vm.$u.api = {
+		register,
 		login,
 		scanCode,
 		startService,
+		buildForm,
+		getMyAppointment,
 	}
 }
 export default {install}
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,7 +2,8 @@
 	<view class="content">
 		<u-button type="primary" @click="scanToSignIn">扫码签到</u-button>
 		<u-button type="primary" @click="scanToStartService">扫码开始服务</u-button>
-		<u-button type="primary">我的工单</u-button>
+		<u-button type="primary" @click="jumpToMyForms">我的工单</u-button>
+		<!-- <u-button type="primary" @click="login">登录</u-button> -->
 	</view>
 </template>
 
@@ -10,10 +11,12 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				username: "",//保持登录状态未写
 			}
 		},
-		onLoad() {
+		onLoad(params) {
+			this.username = params['username']
+			console.log("onLoad:"+this.username)
 		},
 		methods: {
 			scanToSignIn(){
@@ -34,21 +37,44 @@
 			},
 			scanToStartService(){
 				let that = this
+				let res0 = null
 				uni.scanCode({	
 					onlyFromCamera:false,
 					scanType:['qrCode'],
 					success(res) {
-						console.log(res)
-						console.log(res['result'])
+						res0 = res
 						that.$u.api.startService(res.result).then(res=>{
 							uni.showToast({
 								title:"成功开始服务",
 								type:'success',
 							})
+							if(res.code == "200")
+							{
+								console.log("事件表入口数据：",res0.result,that.username)
+								that.$u.api.buildForm(res0.result,that.username).then(res=>{
+									console.log(res)
+									uni.showToast({
+										title:res.msg,
+										type:'success',
+									})
+								})
+							}
 						})
 					}
 				})
-			}
+			},
+			//test
+			// login(){
+			// 	uni.redirectTo({
+			// 		url:"../login/login"
+			// 	})
+			// },
+			jumpToMyForms(){
+				uni.navigateTo({
+					url:"../myForms/myForms?username="+this.username,
+				})
+			},
+			
 		}
 	}
 </script>
